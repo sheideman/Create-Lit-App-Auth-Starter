bb24a63b3959eab02372516e0f3b0c5744e3de11
Steve Heideman <steve@mintsocial.com>
2018-09-29 09:30:39 -0700

    added config.default.js

---
+++ config.default.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  // App name.
+  appName: 'Create Lit App Auth Starter',
+  // Secret for cookie sessions.
+  secret: 'YOURSECRETHERE',
+  mongoUrl: 'mongodb://localhost/test'
+};
